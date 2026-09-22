aa1c066915a2ad81db690822c8cf160f9622d16a
Jay Sharma <jay.sharma@remotestate.in>
2026-09-22 15:16:08 +0530

    included noteList and noteDetails section

--- src/components/NotesList.tsx
+++ src/components/NotesList.tsx
@@ -2,10 +2,62 @@
 
 export function NoteList(){
     return(
-        <div className="noteList bg-[rgba(28,28,28,1)] w-md">
-          <h1 className="text-white p-3">Personal</h1>
+        <div className="noteList bg-[rgba(28,28,28,1)] w-xl overflow-auto">
+          <h1 className="text-white p-3 pb-6">Personal</h1>
 
-          <div className="flex flex-col gap-6">
+          <div className="flex flex-col pl-3 pr-3 gap-6">
+
+            <div className="list1 bg-[rgba(255,255,255,0.03)] flex flex-col h-22 gap-2 justify-center rounded-xs">
+                <p className="text-white pl-3 text-sm">My Goals for the Next Year</p>
+                <div className="flex gap-3 pl-3">
+                    <p className="text-[rgba(255,255,255,0.4)] text-xs">31/12/2022</p>
+                    <p className="text-[rgba(255,255,255,0.6)] text-xs">As the year comes to a...</p>
+                </div>
+            </div>
+
+
+            <div className="list1 bg-[rgba(255,255,255,0.03)] flex flex-col h-22 gap-2 justify-center rounded-xs">
+                <p className="text-white pl-3 text-sm">My Goals for the Next Year</p>
+                <div className="flex gap-3 pl-3">
+                    <p className="text-[rgba(255,255,255,0.4)] text-xs">31/12/2022</p>
+                    <p className="text-[rgba(255,255,255,0.6)] text-xs">As the year comes to a...</p>
+                </div>
+            </div>
+
+
+            <div className="list1 bg-[rgba(255,255,255,0.03)] flex flex-col h-22 gap-2 justify-center rounded-xs">
+                <p className="text-white pl-3 text-sm">My Goals for the Next Year</p>
+                <div className="flex gap-3 pl-3">
+                    <p className="text-[rgba(255,255,255,0.4)] text-xs">31/12/2022</p>
+                    <p className="text-[rgba(255,255,255,0.6)] text-xs">As the year comes to a...</p>
+                </div>
+            </div>
+
+
+            <div className="list1 bg-[rgba(255,255,255,0.03)] flex flex-col h-22 gap-2 justify-center rounded-xs">
+                <p className="text-white pl-3 text-sm">My Goals for the Next Year</p>
+                <div className="flex gap-3 pl-3">
+                    <p className="text-[rgba(255,255,255,0.4)] text-xs">31/12/2022</p>
+                    <p className="text-[rgba(255,255,255,0.6)] text-xs">As the year comes to a...</p>
+                </div>
+            </div>
+
+
+            <div className="list1 bg-[rgba(255,255,255,0.03)] flex flex-col h-22 gap-2 justify-center rounded-xs">
+                <p className="text-white pl-3 text-sm">My Goals for the Next Year</p>
+                <div className="flex gap-3 pl-3">
+                    <p className="text-[rgba(255,255,255,0.4)] text-xs">31/12/2022</p>
+                    <p className="text-[rgba(255,255,255,0.6)] text-xs">As the year comes to a...</p>
+                </div>
+            </div>
+
+            <div className="list1 bg-[rgba(255,255,255,0.03)] flex flex-col h-22 gap-2 justify-center rounded-xs">
+                <p className="text-white pl-3 text-sm">My Goals for the Next Year</p>
+                <div className="flex gap-3 pl-3">
+                    <p className="text-[rgba(255,255,255,0.4)] text-xs">31/12/2022</p>
+                    <p className="text-[rgba(255,255,255,0.6)] text-xs">As the year comes to a...</p>
+                </div>
+            </div>
 
             
 
